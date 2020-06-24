692f69e7ce11698f3e38f1b6d19a7871e91ba1a8
SaurabDey <saurab.jeet@gmail.com>
2020-06-24 10:22:33 +0530

    Framework Second Commit

--- src/test/java/org/student/methods/StudentFactory.java
+++ src/test/java/org/student/methods/StudentFactory.java
@@ -2,6 +2,13 @@ package org.student.methods;
 
 import static io.restassured.RestAssured.given;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.simple.JSONObject;
+
+import com.github.javafaker.Faker;
+
 import io.qameta.allure.Step;
 import io.restassured.response.Response;
 
@@ -9,11 +16,59 @@ public class StudentFactory
 {
 
 	@Step("Student:Get Information with end point {0}")
-	public static void getInformation(String endpoint)
+	public static Response getInformation(String endpoint)
 	{
 		Response resp = given().contentType("application/json").when().get(endpoint);
-
-		resp.then().log().all();
+		return resp;
+	
 	}
 	
+	@Step("Student:Post Information with {0} {1} {2} {3} {4} with end point {5}")
+	public static Response postInformation(String firtname,String lastname,String emailID, String prog,List<String> courses, String endpoint)
+	{
+		StudentClass student = new StudentClass();
+		student.setFirstName(firtname);
+		student.setLastName(lastname);
+		student.setEmail(emailID);
+		student.setProgramme(prog);
+		student.setCourses(courses);
+		
+		Response resp =given().contentType("application/json").body(student).when().post(endpoint);
+		return resp;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Step("Student:Put Information with {0} {1} {2} {3} {4} with end point {5}")
+	public static Response updateInformation(String firstName, String lastName, String emailAddress, String title,
+			List<String> courses, String endpoint) {
+		
+		JSONObject student= new JSONObject();
+		student.put("firstName", firstName);
+		student.put("lastName", lastName);
+		student.put("email", emailAddress);
+		student.put("programme", title);
+		student.put("courses", courses);
+
+		Response resp =given().contentType("application/json").body(student).when().put(endpoint);
+		return resp;
+	}
+	
+	@Step("Student:Patch Information with {0} with end point {1}")
+	public static Response patchInformation( String emailAddress,String endpoint)
+	{
+		StudentClass stu = new StudentClass();
+		stu.setEmail(emailAddress);
+		
+		Response resp = given().contentType("application/json").body(stu).when().patch(endpoint);
+		return resp;
+	
+	}
+	
+	@Step("Student:Delete Information with end point {0}")
+	public static Response deteleInformation(String endpoint)
+	{
+		Response resp = given().contentType("application/json").when().delete(endpoint);
+		return resp;
+	
+	}
 }
--- src/test/java/org/student/methods/StudentTestClass.java
+++ src/test/java/org/student/methods/StudentTestClass.java
@@ -1,7 +1,16 @@
 package org.student.methods;
 
+import static io.restassured.RestAssured.given;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.com.APITestFramework.BaseClass;
+import org.json.simple.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import com.github.javafaker.Faker;
 
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
@@ -11,57 +20,83 @@ import io.qameta.allure.Link;
 import io.qameta.allure.Step;
 import io.qameta.allure.Story;
 import io.qameta.allure.TmsLink;
+import io.restassured.response.Response;
 
 @Epic("Epic1 : Testing Allure Report")
 @Feature("Feature 1: Testing Allure Report")
-public class StudentTestClass extends BaseClass{
+public class StudentTestClass extends BaseClass {
 	@TmsLink("https://hangouts.google.com")
 	@Issue("https://www.seleniumeasy.com/")
 	@Link("https://docs.qameta.io/allure/#_testng")
 	@Description("Description 1 : Testing Allure Report")
 	@Story("Story 1 : Testing Allure Report")
-	
+
 	@Step("Get all student information")
-	@Test
-	public void getStudentMethod()
-	{
+	@Test(priority=1)
+	public void getStudentMethod() {
 
-		StudentFactory.getInformation("/list");
-		
+		Response response = StudentFactory.getInformation("/student/list");
+		response.then().log().all();
+		Assert.assertEquals(response.getStatusCode(), 200);
+
 	}
-	
+
 	@Step("Get info of one student")
-	@Test
-	public void getIndividualStudentMethod()
-	{
+	@Test(priority=2)
+	public void getIndividualStudentMethod() {
 
-		StudentFactory.getInformation("/100");
-		
+		Response response = StudentFactory.getInformation("/student/100");
+		response.then().log().all();
+		Assert.assertEquals(response.getStatusCode(), 200);
+
 	}
-	
+
 	@Step("Create a student")
-	@Test
-	public void postMethod()
-	{
-		
+	@Test(priority=3)
+	public void postMethod() {
+		Faker faker = new Faker();
+
+		List<String> courses = new ArrayList<String>();
+		courses.add("Accounting");
+		courses.add("Statistics");
+
+		Response response = StudentFactory.postInformation(faker.name().firstName(), faker.name().lastName(),
+				faker.internet().emailAddress(), faker.book().title(), courses, "/student");
+		response.then().log().all();
+		Assert.assertEquals(response.getStatusCode(), 201);
 	}
-	
+
+	@SuppressWarnings("unchecked")
 	@Step("Update a student")
-	@Test
-	public void putMethod()
-	{
-		
+	@Test(priority=4)
+	public void putMethod() {
+		Faker faker = new Faker();
+
+		List<String> courses = new ArrayList<String>();
+		courses.add("Accounting");
+		courses.add("Statistics");
+
+		Response response = StudentFactory.updateInformation(faker.name().firstName(), faker.name().lastName(),
+				faker.internet().emailAddress(), faker.book().title(), courses, "/student/101");
+		response.then().log().all();
+		Assert.assertEquals(response.getStatusCode(), 200);
 	}
-	@Step("update email of a student")
-	@Test
-	public void patchMethod()
-	{
-		
+
+	@Step("Update email of a student")
+	@Test(priority=5)
+	public void patchMethod() {
+		Faker faker = new Faker();
+
+		Response response = StudentFactory.patchInformation(faker.internet().emailAddress(), "/student/101");
+		response.then().log().all();
+		Assert.assertEquals(response.getStatusCode(), 200);
 	}
+
 	@Step("Delete a student")
-	@Test
-	public void deleteMethod()
-	{
-		
+	@Test(priority=6)
+	public void deleteMethod() {
+		Response response = StudentFactory.deteleInformation("/student/101");
+		response.then().log().all();
+		Assert.assertEquals(response.getStatusCode(), 204);
 	}
 }
